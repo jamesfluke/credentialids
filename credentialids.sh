95ea224ba73ab1f14bed6109694e07590a27fb3c
jfluckiger <jamesafluke@gmail.com>
2023-01-20 16:37:40 -0700

    Add file

---
+++ credentialids.sh
@@ -0,0 +1,16 @@
+set +x; curl -s -d "script=import com.cloudbees.plugins.credentials.*;def credentialids = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class, Jenkins.instance, null, null );for (c in credentialids) {println(c.id)};" --user $USERNAME:$APITOKEN $JENKINS_URL/scriptText > credentialIds.txt'
+
+
+
+
+
+
+set +x; curl -s -d "
+
+import com.cloudbees.plugins.credentials.*
+def credentialids = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(com.cloudbees.plugins.credentials.common.StandardUsernameCredentials.class, Jenkins.instance, null, null )
+for (c in credentialids) {
+  println(c.id)
+  };
+  
+  --user $USERNAME:$APITOKEN $JENKINS_URL/scriptText > credentialIds.txt'
